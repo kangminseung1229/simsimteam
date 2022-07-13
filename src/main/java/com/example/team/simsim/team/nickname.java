bb96a6907279df0c2ceabc400fdc620269e81196
kangminseung1229 <whelming25@naver.com>
2022-07-13 17:33:19 +0900

    첫 배포

---
+++ src/main/java/com/example/team/simsim/team/nickname.java
@@ -0,0 +1,16 @@
+package com.example.team.simsim.team;
+
+import java.util.HashMap;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+
+import lombok.Data;
+
+@Data
+public class nickname {
+
+    private String value;
+    
+    
+}
